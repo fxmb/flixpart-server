b81f921201bc7d0e8e28bc7ba4959a7db9e46648
Franz Bewerunge <franz@gigs.com>
2022-12-11 22:21:47 +0100

    Fix Medusa

--- medusa-config.js
+++ medusa-config.js
@@ -35,7 +35,9 @@ const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
 const DB_DATABASE = process.env.DB_DATABASE;
 
-const DATABASE_URL = `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+const DATABASE_URL =
+  process.env.DATABASE_URL ||
+  `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
 // Medusa uses Redis, so this needs configuration as well
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
